87d962d70ff721b4ef7d9a26812c7fcda8aff150
Jae Huh <jaeeunhuh@gmail.com>
2017-05-21 18:27:26 +1200

    Save form content in the state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,49 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <UserForm />
+    </div>
   )
 }
 
-export default App
 
+class UserForm extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      name: 'jae',
+      email: 'jae@jae.jae'
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+  handleSubmit (evt) {
+    evt.preventDefault()
+    console.log(this.state.name, this.state.email)
+  }
+
+  handleChange (evt) {
+    const inputName = evt.target.name
+    this.setState({
+      [inputName]: evt.target.value
+    })
+  }
+
+
+  render () {
+    console.log(this.state)
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="name" placeholder="Your Name" onChange={this.handleChange} value={this.state.name} />
+        <input type="email" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} />
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+export default App
